7f6ef0dd8a23b099fef54d4855f3c18b7f339bf5
Akarsh Saklani <117979855+Akarsh-2004@users.noreply.github.com>
2025-03-31 19:20:11 +0530

    Updated for render app.py

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ app = Flask(__name__)
 setup_routes(app)  # Attach routes to Flask app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from waitress import serve  # Waitress is better for Windows/Render
+    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
