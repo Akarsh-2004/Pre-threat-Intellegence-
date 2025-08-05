cf0a77691134e790da2711a072e8d53888080eb0
Akarsh Saklani <117979855+Akarsh-2004@users.noreply.github.com>
2025-08-05 22:29:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from app_routes import setup_routes  # Import function from app_routes.py
+from app_routes import setup_routes
+import os  # <-- MISSING IMPORT
 
 app = Flask(__name__)
-
-setup_routes(app)  # Attach routes to Flask app
+setup_routes(app)
 
 if __name__ == "__main__":
-    from waitress import serve  # Waitress is better for Windows/Render
+    from waitress import serve
     serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
